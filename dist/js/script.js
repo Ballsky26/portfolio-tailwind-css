c9307c7b3713cd9f1a0b4518fa349742a9dd4202
Ballsky22 <ballskyteam22@gmail.com>
2023-02-23 23:12:14 +0700

    Merapikan codingan, menginstall plugin prettier, dan membuat tombol back To Top

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
