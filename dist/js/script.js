a5ef3831c40e64935d024088457003abe53fb026
Ballsky22 <ballskyteam22@gmail.com>
2023-02-23 23:42:50 +0700

    Membuat fitur DarkMode

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,3 +31,15 @@ window.addEventListener("click", function (e) {
     navMenu.classList.add("hidden");
   }
 });
+
+// Darkmode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else {
+    html.classList.remove("dark");
+  }
+});
