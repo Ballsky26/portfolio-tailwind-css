e717cb88f11f0d225ec1963b79439490d537de1e
Ballsky22 <ballskyteam22@gmail.com>
2023-02-23 11:25:16 +0700

    Membuat Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
