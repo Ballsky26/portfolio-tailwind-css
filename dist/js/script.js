bb3cb000e06c0a36a8538187e481ec2bfd579fc6
Ballsky22 <ballskyteam22@gmail.com>
2023-02-23 23:16:08 +0700

    Membuat close hamburger menu dimana saja

--- dist/js/script.js
+++ dist/js/script.js
@@ -23,3 +23,11 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Klik di luar Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
