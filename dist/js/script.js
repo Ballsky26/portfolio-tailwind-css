6dda06049b2da36c358b21fa24e7c711063a65a1
Ballsky22 <ballskyteam22@gmail.com>
2023-02-23 23:50:55 +0700

    Membuat fitur DarkMode Fix!

--- dist/js/script.js
+++ dist/js/script.js
@@ -39,7 +39,20 @@ const html = document.querySelector("html");
 darkToggle.addEventListener("click", function () {
   if (darkToggle.checked) {
     html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
+
+// Pindahkan Posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
